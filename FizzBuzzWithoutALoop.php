a22a90569fe2c8b8817a8fe5de93ff029c39bdbc
ryanorsinger <ryan.orsinger@gmail.com>
2014-11-29 17:52:23 -0600

    Remove .php solution.

--- FizzBuzzWithoutALoop.php
+++
@@ -1,4 +0,0 @@
-// Do FizzBuzz without any loops!
-// Fizzbuzz starts at 1 and goes to 100
-// Multiples of 3 should output "Fizz", Multiples of five should output "Buzz"
-// Multiples of both 3 and 5 output "FizzBuzz", and other numbers output that number's value.

46f585ca09bc9c6f5498bd7c8e61a8d480ca02dd
ryanorsinger <ryan.orsinger@gmail.com>
2014-12-27 01:26:01 -0600

    Add first draft of Sieve of Eratosthenes.

---
+++ SieveOfEratosthenes.php
@@ -0,0 +1,44 @@
+<?php
+
+// Sieve of Eratosthenes steps:
+// Step 1. Create a list of consecutive integers from 2 to n.
+// Step 2. Set p = 2 for the first prime.
+// Step 3. Starting from p, mark its multiples by counting to n in increments of p and mark them in the list.
+// Step 4. Find the first unmarked number greater than p in the list.
+// If there is no such number, stop.
+// Otherwise, let p now equal this new number.
+
+$start = microtime(TRUE);
+
+$n = 13195;
+
+function sieve($n)
+{
+    $prime = 2;
+
+    $list = range($prime, $n);
+
+    while(($prime * 2) < $n)
+    {
+        $primeNumbers[] = $prime;
+
+        $multiples = range($prime, $n, $prime);
+
+        $list = array_diff($list, $multiples);
+
+        $prime = min($list);
+
+    }
+
+    return $primeNumbers;
+};
+
+$primeNumbers = sieve($n);
+
+$end = microtime(TRUE);
+
+$count = count($primeNumbers);
+
+$totalTime = $end - $start;
+
+echo "It took {$totalTime} seconds to generate {$count} prime numbers under n where n={$n}" . PHP_EOL;
